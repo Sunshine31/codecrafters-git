09102073e0ef9c1f492e288af7b4c821df30a793
Sasha Levshin <ocarin31@yandex.ru>
2025-03-02 11:25:58 +0700

    decoding and printing out the object

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::env;
 use std::fs;
+use std::io::Read;
+
+use flate2::read::ZlibDecoder;
 
 fn main() {
-    eprintln!("Logs from your program will appear here!");
+    println!("Logs from your program will appear here!");
 
     let args: Vec<String> = env::args().collect();
     if args[1] == "init" {
@@ -12,8 +15,22 @@ fn main() {
         fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
         println!("Initialized git directory");
     } else if args[1] == "cat-file" {
-        let hash = args[3].as_str();
-        dbg!(hash);
+        if args[2] == "-p" {
+            let hash = args[3].clone();
+            let folder_name = &hash[0..2];
+            let file_name = &hash[2..];
+            let path = format!(".git/objects/{folder_name}/{file_name}");
+            let mut object = fs::File::open(&path).unwrap();
+            let mut content: Vec<u8> = vec![];
+            let mut extracted_content = String::new();
+
+            object.read_to_end(&mut content).unwrap();
+
+            let mut decoder = ZlibDecoder::new(content.as_slice());
+
+            decoder.read_to_string(&mut extracted_content).unwrap();
+            print!("{extracted_content}");
+        }
     } else {
         println!("unknown command: {}", args[1]);
     }
